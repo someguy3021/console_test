00ef4fe5672fcf1e241f6fbb20eece80bd1c1f32
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2024-05-24 21:16:50 +0500

    15.2

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-let i = 1
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const http = require('http');
 
-while (i < 10){
-    i++
-    console.log(i);
-}
+http
+    .createServer((request, response) => {
+        response.setHeader("Content-Type", "text/html; charset=utf-8;");
+        if (request.url === '/'){
+            response.write('Выдал данные');
+            handleData();
+        } else{
+            response.write('А тут не данные');
+            console.log("Nope!");
+        }
+        response.end();
+
+}).listen(3000);
+
+
+async function handleData(){
+    try{
+        const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts');
+
+        fs.writeFile(path.resolve(__dirname, '1.json'), JSON.stringify(data), 'utf-8', (err) => {
+            if(err){
+                throw err;
+            }
+            console.log("Done!");
+        })
+    } catch (error) {
+        console.error(error);
+    }
+};
